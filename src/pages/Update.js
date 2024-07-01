9261cf86e110b003ed2980d78c7c04bcf07850eb
Lu <66273292+thisdotLolu@users.noreply.github.com>
2024-07-01 15:47:48 +0100

    Add files via upload

---
+++ src/pages/Update.js
@@ -0,0 +1,86 @@
+import { useEffect, useState } from "react"
+import { useNavigate, useParams } from "react-router-dom"
+import supabase from "../config/supabaseClient"
+
+const Update = () => {
+  const{id} = useParams()
+  const navigate = useNavigate()
+  const [title, setTitle] = useState('')
+  const [method, setMethod] = useState('')
+  const [rating, setRating] = useState('')
+  const [formError, setFormError] = useState(null)
+
+  useEffect(()=>{
+    const fetchSmoothie=async()=>{
+      const {data,error} = await supabase
+      .from('smoothies')
+      .select()
+      .eq('id',id)
+      .single()
+
+      if(error){
+        navigate('/', {replace:true})
+      }
+      if(data){
+        setTitle(data.title)
+        setMethod(data.method)
+        setRating(data.rating)
+        console.log(data)
+      }
+    }
+
+
+    fetchSmoothie()
+  },[id,navigate])
+
+  const handleSubmit=async(e)=>{
+    e.preventDefault();
+
+
+    const {data,error} = await supabase
+    .from('smoothies')
+    .update({title,method,rating})
+    .eq('id',id)
+    .select()
+
+    if(data){
+      navigate('/')
+    }
+  }
+  return (
+    <div className="page update">
+      <form 
+      onSubmit={handleSubmit}
+      >
+        <label htmlFor="title">Title:</label>
+        <input 
+          type="text" 
+          id="title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+
+        <label htmlFor="method">Method:</label>
+        <textarea 
+          id="method"
+          value={method}
+          onChange={(e) => setMethod(e.target.value)}
+        />
+
+        <label htmlFor="rating">Rating:</label>
+        <input 
+          type="number"
+          id="rating"
+          value={rating}
+          onChange={(e) => setRating(e.target.value)}
+        />
+
+        <button>Create Smoothie Recipe</button>
+
+        {formError && <p className="error">{formError}</p>}
+      </form>
+    </div>
+  )
+}
+
+export default Update
